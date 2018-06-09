cba399a9a63fcec108b2ded5530f994a0658617f
Joana Palero <joana.palero24@gmail.com>
2018-06-09 13:35:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_variety)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
