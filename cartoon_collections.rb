50a82c5e662bd5a8a7d1263f9d071ad8885c93a6
Joana Palero <joana.palero24@gmail.com>
2018-06-09 13:36:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 
 def find_the_cheese(cheese_variety)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_variety.find {|variety| cheese_types.include?()
+  cheese_variety.find {|variety| cheese_types.include?(cheese_variety)}
 end
